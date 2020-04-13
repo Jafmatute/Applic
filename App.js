41b6907753def22379ae08f40636977aad57c87d
Developer <developer@Mac-de-Developer.local>
2020-04-13 11:56:11 -0700

    Implementation of the reac native elements library

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome a applic!</Text>
+      <Button
+  title="Solid Button"
+  icon={
+    <Icon
+    type="material-community"
+      name="check-outline"
+      size={15}
+      color="white"
+    />
+  }
+  
+  />
     </View>
   );
 }
