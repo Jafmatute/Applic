c7a1cd11f7cd72fcb8d19211669493fa9330ba5e
Developer <developer@Mac-de-Developer.local>
2020-04-14 20:48:28 -0700

    Creating the guest user and user login screens of the application

--- app/screens/Account/MyAccount.js
+++ app/screens/Account/MyAccount.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from "react";
-import {View , Text} from "react-native";
 import * as firebase from 'firebase';
 import Loading from "../../components/Loading";
+import  UserGuest  from "./UserGuest";
+import UserLogged from "./UserLogged";
 
 export default function MyAccount({navigation}) {
 
@@ -20,20 +21,10 @@ export default function MyAccount({navigation}) {
     if(login===null){
         return( <Loading isVisible={true} text="Cargando.." /> )
     }
+   
 
-    if(login) {
-        return(
-            <View>
-                <Text>Usuario Logeado!</Text>
-            </View>
-        )
-    }
+    return login ? <UserLogged /> : <UserGuest /> ;
 
-    return(
-
-        <View><Text>No autorizdo.</Text></View>
-    )
-    
 
 }
 
---
+++ app/screens/Account/UserGuest.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import {View, Text} from 'react-native';
+
+export default function UserGuest() {
+
+    return(
+        <View>
+
+        <Text>Usuario Invitado.</Text>
+
+        </View>
+    )
+}
