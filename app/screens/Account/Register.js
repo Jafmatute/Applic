919fd21e5ecfe2160412575bee45dc0677ec06e3
Developer <developer@Mac-de-Developer.local>
2020-04-16 03:32:59 -0700

    Creating structure of the user registration page

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import logo from '../../../assets/img/register.png';
 
 export default function Register({navigation}) {
 
     return(
 
-        <View>
+        <KeyboardAwareScrollView>
+        <Image source={logo}
+               style={stylesRegister.logo}
+               resizeMode='contain'
+         />
 
-            <Text>Registro de usuarios</Text>
+         <View style={stylesRegister.viewForm} >
 
-        </View>
+            <Text>Formulario de registro...</Text>
+
+         </View>
+      </KeyboardAwareScrollView>
     )
 }
+
+const stylesRegister = StyleSheet.create({
+
+    logo:{
+      width:'100%',
+      height:150,
+      marginTop:20  
+    },
+    viewForm:{
+       marginRight:40,
+       marginLeft:40
+
+    }
+})
