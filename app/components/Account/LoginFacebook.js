02a5be13e1248b81a830a1e5d12730e79fac876b
Developer <developer@Mac-de-Developer.local>
2020-04-21 01:17:52 -0700

    Writing the logic to be able to log in using Facebook

--- app/components/Account/LoginFacebook.js
+++ app/components/Account/LoginFacebook.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { SocialIcon } from "react-native-elements";
-
+import * as firebase from "firebase";
+import * as Facebook from "expo-facebook";
+import { FacebookApi } from "../../utils/Social";
+import Loading from "../Loading";
 export default function LoginFacebook() {
-  const loginFacebook = () => {
-    console.log("iniciando sesión con Facebook");
+  const loginFacebook = async () => {
+    await Facebook.initializeAsync();
+
+    const {
+      type,
+      token,
+    } = await Facebook.logInWithReadPermissionsAsync(
+      FacebookApi.application_id,
+      { permissions: FacebookApi.permissions }
+    );
+
+    if (type === "success") {
+      const credentials = firebase.auth.FacebookAuthProvider.credential(token);
+      await firebase
+        .auth()
+        .signInWithCredential(credentials)
+        .then(() => {
+          console.log("login correcto");
+        })
+        .catch((e) => {
+          let errorMsj = e.message;
+          console.log(errorMsj);
+          console.log("error accediendo con Facebook, intentelo más tarde");
+        });
+    } else if (type === "cancel") {
+      console.log("Inicio de sesión con Facebook cancelado");
+    } else {
+      console.log("error desconocido, intentelo más tarde");
+    }
   };
 
   return (
---
+++ app/utils/Social.js
@@ -0,0 +1,4 @@
+export const FacebookApi = {
+  application_id: "650719582437268",
+  permissions: ["public_profile"],
+};
