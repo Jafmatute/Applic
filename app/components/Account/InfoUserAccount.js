ebb3cd7e8bf282373f14a4a64abf69944aadf998
Developer <developer@Mac-de-Developer.local>
2020-04-25 10:55:07 -0700

    Creating user component that login and fetching data from firebase

---
+++ app/components/Account/InfoUserAccount.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Avatar } from "react-native-elements";
+
+export default function InfoUserAccount(props) {
+  console.log(props);
+  return (
+    <View>
+      <Text>Usuario Logeado componente</Text>
+    </View>
+  );
+}
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Button } from "react-native-elements";
 import * as firebase from "firebase";
+import InfoUserAccount from "../../components/Account/InfoUserAccount";
 
 export default function UserLogged() {
+  const [infoUser, setInfoUser] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const user = await firebase.auth().currentUser;
+      //console.log("desde usuario logeado", user);
+      setInfoUser[user.providerData[0]];
+    })();
+  }, []);
+
   return (
     <View>
-      <Text>Usuario Logeado!</Text>
+      <InfoUserAccount infoUser={infoUser} />
 
       <Button title="Cerrar sesión" onPress={() => firebase.auth().signOut()} />
     </View>
